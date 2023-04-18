01e917fe80bb5d8dcbabdb0ab556c1ef9b283b40
alexangria1992 <alex040892@gmail.com>
2023-04-18 16:36:24 +1200

    Desktop and Mobile Navigation

---
+++ pages/Components/Navbar/index.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import style from "./style.module.css";
+import Image from "next/image";
+
+export default function Navbar() {
+  const [offCanvas, setOffCanvas] = useState(false);
+  return (
+    <>
+      <nav className={style.nav}>
+        <a href="">Arx</a>
+        <Image src="/img/logo.svg" width={32} height={32} alt="" />
+        <Image
+          src="/img/menu.svg"
+          width={32}
+          height={32}
+          style={{ cursor: "pointer" }}
+          onClick={() => setOffCanvas(true)}
+          alt=""
+        />
+      </nav>
+
+      <div className={`${style.canvas_menu} ${offCanvas ? style.active : ""} `}>
+        <div className={style.close_icon}>
+          <Image
+            src="/img/close.svg"
+            width={32}
+            height={32}
+            alt=""
+            onClick={() => setOffCanvas(false)}
+          />
+        </div>
+        <div className={style.canvas_content}>
+          <div className={style.social}>
+            <h4>Social</h4>
+            <div>
+              <p>
+                <img src="/img/Instagram.svg" alt="" />
+                <span>&nbsp; Instagram</span>
+              </p>
+              <p>
+                <img src="/img/Twitter.svg" alt="" />
+                <span>&nbsp; Twitter</span>
+              </p>
+              <p>
+                <img src="/img/Youtube.svg" alt="" />
+                <span>&nbsp; Youtube</span>
+              </p>
+              <p>
+                <img src="/img/Facebook.svg" alt="" />
+                <span>&nbsp; Facebook</span>
+              </p>
+              <p>
+                <img src="/img/Dribbble.svg" alt="" />
+                <span>&nbsp; Dribble</span>
+              </p>
+              <p>
+                <img src="/img/Linkedin.svg" alt="" />
+                <span>&nbsp; LinkedIn</span>
+              </p>
+            </div>
+          </div>
+          <div className={style.menu}>
+            <h4>Menu</h4>
+            <p style={{ marginTop: 0 }}>
+              <a href="#home">Home</a>
+            </p>
+            <p>
+              <a href="#featured">Featured</a>
+            </p>
+            <p>
+              <a href="#projects">Projects</a>
+            </p>
+            <p>
+              <a href="#testimonials">Testimonials</a>
+            </p>
+          </div>
+        </div>
+        <div className={style.email}>
+          <p>Get in touch</p>
+          <h2>
+            <u>Info@arx.com</u>
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        Arx - Architecture Landing Page
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
