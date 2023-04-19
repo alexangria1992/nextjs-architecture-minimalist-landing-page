73fc1bb9734f490d3061963c5b4b7a5ded3e84d1
alexangria1992 <alex040892@gmail.com>
2023-04-19 21:24:12 +1200

    Desktop and Mobile Step section

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from "@/styles/Home.module.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Featured from "./Components/Featured";
+import Step from "./Components/Step";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Featured />
+      <Step />
     </>
   );
 }
